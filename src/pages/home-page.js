1cbb1afae3013ba927081b194b237a168123d6e6
Tomasz Knapik <hi@tmkn.org>
2019-04-23 17:25:32 +0100

    Add homepage markup

--- src/pages/home-page.js
+++ src/pages/home-page.js
@@ -4,6 +4,7 @@ import { graphql, Link } from 'gatsby';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import RichText from '../components/rich-text';
 
 const HomePage = ({ data }) => {
   const page = data.wagtail.pages.base.homePage[0];
@@ -29,14 +30,140 @@ const HomePage = ({ data }) => {
     </>;
   };
 
+  const heroContainerStyle = {};
+  const heroImage = page.image && page.image.rendition.url;
+  const promoImage = page.promoImage && page.promoImage.rendition;
+  if (heroImage) {
+    heroContainerStyle['backgroundImage'] = `url(${heroImage})`;
+  }
+
   return <Layout>
     <SEO title={page.seoTitle} description={page.seoDescription} />
-    <h1>{page.title}</h1>
-    <p>Page statically generated with data provided from Wagtail CMS.</p>
-    <a href="https://wagtail.io">
-      <img src={require('../images/wagtail.svg')} style={{ width: '50%' }} />
-    </a>
-    {renderPageList(page.children, 'See')}
+    <div style={heroContainerStyle}>
+      <h1>{page.title}</h1>
+      <p>{page.heroText}</p>
+      <Link to={page.heroCtaLink.url}>{page.heroCta}</Link>
+    </div>
+    <div className="container">
+      <div className="row promo-row">
+        <div className="col-sm-5 promo">
+          {(promoImage || page.promoTitle || page.promoText) && <>
+            <figure><img src={promoImage.url} alt={promoImage.alt} /></figure>
+            <div className="promo-text">
+              <h3>{page.promoTitle}</h3>
+              <RichText>{page.promoText}</RichText>
+            </div>
+          </>}
+        </div>
+
+        <div className="col-sm-6 col-sm-offset-1 feature-1">
+          {page.featuredSection1 &&
+            <>
+              <h2>{page.featuredSection1Title}</h2>
+              <div className="featured-children">
+                {page.featuredSection1.children.map(v => {
+                  const image = v.specific.image && v.specific.image.rendition;
+                  let imageElement;
+                  if (image) {
+                        imageElement = (
+                          <Link to={v.url}>
+                            <figure>
+                              <img src={image.url} alt={image.alt} />
+                            </figure>
+                          </Link>
+                        );
+                  }
+
+                  return <li key={v.id}>
+                    <div className="row">
+                      <div className="col-xs-4">
+                        {imageElement}
+                      </div>
+                      <div className="col-xs-8">
+                        <h3><Link to={v.url}>{v.title}</Link></h3>
+                      </div>
+                    </div>
+                  </li>;
+                })}
+              </div>
+            </>
+          }
+        </div>
+
+        {page.body && <div>{JSON.stringify(page.body)}</div>}
+
+        <div className="col-sm-6 col-sm-offset-1 feature-1">
+          {page.featuredSection2 &&
+            <>
+              <h2>{page.featuredSection2Title}</h2>
+              <div className="featured-children">
+                {page.featuredSection2.children.map(v => {
+                  const image = v.specific.image && v.specific.image.rendition;
+                  let imageElement;
+                  if (image) {
+                        imageElement = (
+                          <Link to={v.url}>
+                            <figure>
+                              <img src={image.url} alt={image.alt} />
+                            </figure>
+                          </Link>
+                        );
+                  }
+
+                  return <li key={v.id}>
+                    <div className="row">
+                      <div className="col-xs-4">
+                        {imageElement}
+                      </div>
+                      <div className="col-xs-8">
+                        <h3><Link to={v.url}>{v.title}</Link></h3>
+                        {v.specific && v.specific.introduction &&
+                          <p>{v.specific.introduction}</p>
+                        }
+                      </div>
+                    </div>
+                  </li>;
+                })}
+              </div>
+            </>
+          }
+        </div>
+
+        <div className="col-sm-6 col-sm-offset-1 feature-1">
+          {page.featuredSection3 &&
+            <>
+              <h2>{page.featuredSection3Title}</h2>
+              <div className="featured-children">
+                {page.featuredSection3.children.map(v => {
+                  const image = v.specific.image && v.specific.image.rendition;
+                  let imageElement;
+                  if (image) {
+                        imageElement = (
+                          <Link to={v.url}>
+                            <figure>
+                              <img src={image.url} alt={image.alt} />
+                            </figure>
+                          </Link>
+                        );
+                  }
+
+                  return <li key={v.id}>
+                    <div className="row">
+                      <div className="col-xs-4">
+                        {imageElement}
+                      </div>
+                      <div className="col-xs-8">
+                        <h3><Link to={v.url}>{v.title}</Link></h3>
+                      </div>
+                    </div>
+                  </li>;
+                })}
+              </div>
+            </>
+          }
+        </div>
+      </div>
+    </div>
   </Layout>;
 };
 
@@ -59,6 +186,87 @@ export const query = graphql`
               title
               url
             }
+
+            heroText
+            heroCta
+            heroCtaLink {
+              url
+            }
+            image {
+              rendition(filter: "fill-1920x600") {
+                url
+              }
+            }
+
+            promoTitle
+            promoText
+            promoImage {
+              rendition(filter: "fill-200x200-c100") {
+                url
+                alt
+              }
+            }
+
+            featuredSection1 {
+              children(limit:4) {
+                id
+                url
+                title
+                specific {
+                  ... on WAGTAIL_BreadsBreadPageObjectType {
+                    image {
+                      rendition(filter: "fill-180x140-c100") {
+                        url
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            featuredSection1Title
+
+            body
+
+            featuredSection2 {
+              children(limit: 3) {
+                id
+                url
+                title
+                specific {
+                  ... on WAGTAIL_LocationsLocationPageObjectType {
+                    image {
+                      rendition(filter: "fill-180x140-c100") {
+                        url
+                      }
+                    }
+                    introduction
+                  }
+                }
+              }
+            }
+
+            featuredSection2Title
+
+
+            featuredSection3 {
+              children(limit:6) {
+                id
+                title
+                url
+                specific {
+                  ... on WAGTAIL_BlogBlogPageObjectType {
+                    image {
+                      rendition(filter: "fill-180x140-c100") {
+                        url
+                      }
+                    }
+                    introduction
+                  }
+                }
+              }
+            }
+
+            featuredSection3Title
           }
         }
       }
